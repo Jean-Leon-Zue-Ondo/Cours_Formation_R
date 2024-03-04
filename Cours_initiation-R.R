bb13862dadb8cc98cec001d2ead0099d0f7780b8
Jean-Leon <zjeanleon@yahoo.fr>
2024-03-04 11:32:20 +0100

    Introduction et synthaxe en R

---
+++ Cours_initiation-R.R
@@ -0,0 +1,27 @@
+
+# Plan de formation
+  
+#Introduction
+#Structure des données
+#Les graphiques
+#Statistique avec R
+
+# -----------------------------------------
+
+# Introduction
+
+"Hello World"
+
+# Pour afficher des nombres :
+ 
+5
+6
+10
+
+## Compilation du code
+
+### Exemple
+
+"Hello World!"
+
+# la `print()`fonction 
